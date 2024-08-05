6c176f686cf02efb5b708ecf67978cab88bc6c50
Pjmaciel <pjmaciel_4@hotmail.com>
2024-08-05 18:10:09 -0300

    feat(Question): Add model Question with validation of name unique.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_05_195412) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_05_201218) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,6 +46,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_05_195412) do
     t.string "nome"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["nome"], name: "index_formularies_on_nome", unique: true
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "name"
+    t.bigint "formulary_id", null: false
+    t.text "question_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["formulary_id"], name: "index_questions_on_formulary_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -70,5 +80,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_05_195412) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "questions", "formularies"
   add_foreign_key "visits", "users"
 end
