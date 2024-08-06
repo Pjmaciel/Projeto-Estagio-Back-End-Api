f2816826b4eca2685ffd1cdd1e7b8b4fa1308fd6
Pjmaciel <pjmaciel_4@hotmail.com>
2024-08-06 19:25:42 -0300

    feat(controller): add controllers for models

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,44 @@
+class AnswersController < ApplicationController
+  before_action :set_answer, only: [:show, :update, :destroy]
+
+  def index
+    @answers = Answer.all
+    render json: @answers
+  end
+
+  def show
+    render json: @answer
+  end
+
+  def create
+    @answer = Answer.new(answer_params)
+
+    if @answer.save
+      render json: @answer, status: :created, location: @answer
+    else
+      render json: @answer.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @answer.update(answer_params)
+      render json: @answer
+    else
+      render json: @answer.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @answer.destroy
+  end
+
+  private
+
+  def set_answer
+    @answer = Answer.find(params[:id])
+  end
+
+  def answer_params
+    params.require(:answer).permit(:content, :formulary_id, :question_id, :visit_id, :answered_at)
+  end
+end
