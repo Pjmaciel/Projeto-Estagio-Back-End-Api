7ed3b626c56a3be46ec2a35da986ca1ec927ea32
Pjmaciel <pjmaciel_4@hotmail.com>
2024-08-07 16:17:51 -0300

    fix(visit): change status enum to string for visit model

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -5,11 +5,19 @@ class Visit < ApplicationRecord
     PENDING: 'PENDENTE',
     REALIZING: 'REALIZANDO',
     CARRIED_OUT: 'REALIZADA'
-  }
+  }, _suffix: true
 
   validates :date, presence: true, comparison: { greater_than_or_equal_to: -> { Date.current } }
   validates :status, presence: true, inclusion: { in: statuses.keys.map(&:to_s) }
   validates :checkin_at, presence: true, comparison: { less_than: :checkout_at }
   validates :checkout_at, presence: true, comparison: { greater_than: :checkin_at }
   validates :user_id, presence: true
+
+  before_save :debug_status
+
+  private
+
+  def debug_status
+    Rails.logger.debug "Status before save: #{self.status}"
+  end
 end
